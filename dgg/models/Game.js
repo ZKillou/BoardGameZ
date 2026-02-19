2051645c1e53dca33aaf8733e2a4639a1501512a
ZKillou <2econtedekillian@gmail.com>
2026-02-19 19:55:55 +0100

    [DGG] rallonge

--- dgg/models/Game.js
+++ dgg/models/Game.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const gameSchema = new mongoose.Schema({
   started: { type: Boolean, default: false },
   startTime: Date,
-  duration: { type: Number, default: 3600000 } // 1h
+  duration: { type: Number, default: 3600000 * 2 } // 2h
 })
 
 gameSchema.virtual('endTime').get(function() {
